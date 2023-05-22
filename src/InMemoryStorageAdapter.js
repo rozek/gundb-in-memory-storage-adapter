831a66a4e4b7d9113eb4094715023e6ba3d572d4
Andreas Rozek <A.Rozek@gmx.de>
2023-05-22 17:52:41 +0200

    Update InMemoryStorageAdapter.js

--- src/InMemoryStorageAdapter.js
+++ src/InMemoryStorageAdapter.js
@@ -4,18 +4,18 @@
 
   GUN.on('create', function (Context) {        // do not use a "fat arrow" here!
     this.to.next(Context)
-    
+
     if (Context.opt.inMemory != true) { return }     // adapter wasn't requested
-    
+
   /**** get - retrieve the contents of a given node ****/
-    
+
     Context.on('get', function (WireMessage) {           // no "fat arrow" here!
       this.to.next(WireMessage)
-      
+
       let LEX  = WireMessage.get
       let Soul = (LEX == null ? undefined : LEX['#'])
       if (Soul == null) { return }
-      
+
       let Data = InMemoryStorage[Soul]              // fetches data from storage
       if (Data != null) {
         let Key = LEX['.']
@@ -25,26 +25,27 @@
           )
         }
       }
-      
+
       GUN.on.get.ack(WireMessage,Data)
     })
-        
+
   /**** put - patches the contents of a given node ****/
 
     Context.on('put', function (WireMessage) {           // no "fat arrow" here!
       this.to.next(WireMessage)
 
-      let LEX  = WireMessage.put
-      let Soul = LEX['#'], Key  = LEX['.']
-      let Id   = WireMessage['#']
-      let Ok   = WireMessage.ok || ''
+      let LEX     = WireMessage.put
+      let Soul    = LEX['#'], Key  = LEX['.']
+      let DedupId = WireMessage['#']
+      let Ok      = WireMessage.ok || ''
 
       InMemoryStorage[Soul] = GUN.state.ify(     // merges new data into storage
         InMemoryStorage[Soul], Key, LEX['>'], LEX[':'], Soul
       )
-      
+
       Context.on('in', {                           // acknowledge put completion
-        '@':Id, err:undefined, ok:1
+        '@':DedupId, err:undefined, ok:1
       })
     })
   })
+
